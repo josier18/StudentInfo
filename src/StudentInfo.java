33b301cd775d35650c13cb81352a88072d144f74
josier18 <joseline22729@gmail.com>
2023-08-26 23:39:43 -0500

    first commit

---
+++ src/StudentInfo.java
@@ -0,0 +1,16 @@
+//**********************************
+// COSC 1173_03 Programming Lab
+// Name: Joseline Robles
+// Date: 8/26/2023
+// Display personal information
+//**********************************
+
+public class StudentInfo {
+    public static void main(String[] args) 
+     {
+        System.out.println("Student Information");
+        System.out.println("Name: Joseline Robles");
+        System.out.println("Major: Computer Science");
+        System.out.println("Hobbies: reading, drawing, playing guitar");
+    }
+}
